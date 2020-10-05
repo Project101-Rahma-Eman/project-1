46f66a0f3d2ef9cb9a424ec5d0a45553b65e283a
emansawalha <70060108+emansawalha@users.noreply.github.com>
2020-10-05 13:04:33 +0300

    last updates

--- action.js
+++ action.js
@@ -1,95 +1,80 @@
 var radioValue = $("input[name='gender']:checked").val();
 var fgender =$("input[value='l']").val();
- var height = $("#inputHeight").val();
-
-
 $(document).ready(function(){
 	
 $("#fgender").click(function(){
 		$("#perfectWeight").hover(function(){
-
 		if($("#inputHeight").val()===''){
 			alert("please enter your height && weight")
-		}else{ 
+		}else{
 		
 		var heigh= $("#inputHeight").val()
-
-         var perfectWeight =heigh -110 
+		 $("inputHeight").click(function() {
+                alert($.isNumeric(heigh ));
+            });
+         var perfectWeight =heigh -110
 		
 		$("#perfectWeight").val( perfectWeight);
-
-
-
+	}
 	
-}
+})
 })
 	})
-})
 //$(document).ready(function(){
 	//debugger
 	
 $("#mgender").click(function(){
 		$("#perfectWeight").hover(function(){
-
 		if($("#inputHeight").val()===''){
 			alert("please enter your height && weight")
-		}else{ 
+		}else{
 		
 		var heigh= $("#inputHeight").val()
-
-         var perfectWeight =heigh -100 
+         var perfectWeight =heigh -100
 		
 		$("#perfectWeight").val( perfectWeight);
-
-
-
 	
 }
 })
 	})
-
-		//$("#current").hover(function calcBMI(){
-
-		
-		//	var weight = $("#current").val();
-	    //var height = $("#inputHeight").val();
-		//	var bmi =( weight / (height*height))*10000;
-		//	var x = $("#yourBMI").val(bmi);
-		
-
-			
-
-			
-			
-			
-//})
-
-$(document).ready(function(){
-    $("BMI").click(function() {
-       // var weight = $("weight").value;
-       // var height = $("height").value;
-       $("#current").hover(function calcBMI(){
-
+		$("#yourBMI").hover(function(){
 		
 			var weight = $("#current").val();
-	    //var height = $("#inputHeight").val();
+	    var height = $("#inputHeight").val();
 			var bmi =( weight / (height*height))*10000;
-		//	var x = $("#yourBMI").val(bmi);
-		
-        if (weight > 0 && height > 0) {
-            //var bmi = (weight / (height * height)) ;
-            $("yourBMI") = bmi;
-            if (calcBMI() < 18.5) {
-                $("status").value = "You are underweight.";
-            }
-            if (calcBMI() > 18.5 && calcBMI() < 24.9) {
-                $("status").value = "You are normal.";
-            }
-            if (calcBMI() > 24.9 && calcBMI() < 29.99) {
-                $("status").value = "You are overweight.";
-            }
-        } else {
-            $("status").value = "You are obese.";
-            }   
-    });
-});
+			$("#yourBMI").val(bmi.toFixed(0));
+			if($("#yourBMI").val()< 16){
+				$("#status").val(" severely under wieght");
+				
+			}else if($("#yourBMI").val()< 18){
+				$("#status").val("  under wieght");
+				
+			}else if($("#yourBMI").val()< 24.9){
+				$("#status").val(" normal");
+				
+			}else if($("#yourBMI").val()< 29.9){
+				$("#status").val("over wight");
+				
+			}else if($("#yourBMI").val()< 34.9){
+				$("#status").val("obese");
+				
+			}else if($("#yourBMI").val()> 35){
+			$("#status").val("severely obese");}
+			
+			
+			
+			
+			
+			
+			
+			
+})
+		//input only numbers in text box
+  $("#container").keypress(function(e) {
+      var arr = [];
+      var kk = e.which;
+      for (i = 48; i < 58; i++)
+          arr.push(i);
+      if (!(arr.indexOf(kk)>=0))
+          e.preventDefault();
+  });
