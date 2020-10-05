cefc670b97599f430b232631f46452ad0fc206bc
rahmeh alomari <rahmehalomari577@gmail.com>
2020-10-05 17:37:21 +0300

    project101

--- action.js
+++ action.js
@@ -1,66 +1,53 @@
 var radioValue = $("input[name='gender']:checked").val();
 var fgender =$("input[value='l']").val();
-
- 
-
 $(document).ready(function(){
-	
+	//this function to calculate the perfect weight 
 $("#fgender").click(function(){
+	//calculate the perfect weight after gender selection female  
 		$("#perfectWeight").hover(function(){
-
+			//alert user to input the hight 
 		if($("#inputHeight").val()===''){
 			alert("please enter your height && weight")
-		}else{ 
-		
+		}else{
+		//calculate the perfect weight 
 		var heigh= $("#inputHeight").val()
-		 $("inputHeight").click(function() { 
-                 
-                alert($.isNumeric(heigh )); 
-            }); 
-
-         var perfectWeight =heigh -110 
+		 $("inputHeight").click(function() {
+                alert($.isNumeric(heigh ));
+            });
+         var perfectWeight =heigh -110
 		
 		$("#perfectWeight").val( perfectWeight);
 	}
-
-
-
 	
 })
 })
 	})
-
-//$(document).ready(function(){
-	//debugger
-	
+​
+	//calculate the perfect weight after gender selection male
 $("#mgender").click(function(){
 		$("#perfectWeight").hover(function(){
-
+			//alert user to input the hight 
 		if($("#inputHeight").val()===''){
 			alert("please enter your height && weight")
-		}else{ 
-		
+		}else{
+		//calculate the perfect weight 
 		var heigh= $("#inputHeight").val()
-
-         var perfectWeight =heigh -100 
+         var perfectWeight =heigh -100
 		
 		$("#perfectWeight").val( perfectWeight);
-
-
-
 	
 }
 })
 	})
-
+        // calulate the BMI 
 		$("#yourBMI").hover(function(){
-
-
 		
 			var weight = $("#current").val();
 	    var height = $("#inputHeight").val();
 			var bmi =( weight / (height*height))*10000;
 			$("#yourBMI").val(bmi.toFixed(0));
+			//check the BMI Status 
+​
 			if($("#yourBMI").val()< 16){
 				$("#status").val(" severely under wieght");
 				
@@ -73,45 +60,20 @@ $("#mgender").click(function(){
 			}else if($("#yourBMI").val()< 29.9){
 				$("#status").val("over wight");
 				
-			}else if($("#yourBMI").val()<= 34.9){
+			}else if($("#yourBMI").val()< 34.9){
 				$("#status").val("obese");
 				
-			}else if($("#yourBMI").val()> 34.9){
+			}else if($("#yourBMI").val()> 35){
 			$("#status").val("severely obese");}
-			
-
-
-			
-
-			
-
-
-			
-
-			
-
-			
-			
+					
 			
 })
-		//input only numbers in text box 
+		//input only numbers in text box
   $("#container").keypress(function(e) {
       var arr = [];
       var kk = e.which;
-   
       for (i = 48; i < 58; i++)
           arr.push(i);
-   
       if (!(arr.indexOf(kk)>=0))
           e.preventDefault();
   });
-
-		
-	
-	
-   
-
-
-
-
-
